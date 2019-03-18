d87a167edb6e7a0236abfe2acd1e86611bdd7c0e
Ahmed Zaki <ahmedzaki@192.168.58.128>
2019-03-18 16:12:26 +0200

    Initial Commit

---
+++ Clinic Task/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Clinic Task
+//
+//  Created by Ahmed Zaki on 3/18/19.
+//  Copyright © 2019 Ahmed Zaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
