da10252e382ffb2223020c7bf95cd326c58409a4
Ahmed Zaki <ahmedzaki@192.168.58.128>
2019-03-18 16:19:28 +0200

    Create DataModel

---
+++ Clinic Task/Model/Doctor.swift
@@ -0,0 +1,36 @@
+//
+//  Doctor.swift
+//  Clinic Task
+//
+//  Created by Ahmed Zaki on 3/18/19.
+//  Copyright © 2019 Ahmed Zaki. All rights reserved.
+//
+
+import Foundation
+struct Doctor: Codable
+{
+    let id, accountType: Int
+    let name: String
+    let image: String
+    let speciality: String
+    let minFees, accountID: Int
+    let uniqueID: String
+    let isPremium: Int
+    let title: String?
+    let myRecommendsCount, followers, isRecommended: Int
+    
+    enum CodingKeys: String, CodingKey
+    {
+        case id
+        case accountType = "account_type"
+        case name, image, speciality
+        case minFees = "min_fees"
+        case accountID = "account_id"
+        case uniqueID = "unique_id"
+        case isPremium = "is_premium"
+        case title
+        case myRecommendsCount = "my_recommends_count"
+        case followers
+        case isRecommended = "is_recommended"
+    }
+}
